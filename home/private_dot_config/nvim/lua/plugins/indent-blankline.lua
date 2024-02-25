fa9106607c16876b13c5988facaa61c82470a6c4
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-02-24 22:49:02 -0500

    back to indent-blankline

--- home/private_dot_config/nvim/lua/plugins/indent-blankline.lua
+++ home/private_dot_config/nvim/lua/plugins/indent-blankline.lua
@@ -1,10 +1,11 @@
 return {
   {
     "lukas-reineke/indent-blankline.nvim",
-    enabled = false, -- vim.version().minor < 10,
+    enabled = vim.version().minor < 10,
   },
   {
     "echasnovski/mini.indentscope",
+    enabled = false,
     version = false, -- wait till new 0.7.0 release to put it back on semver
     event = "VeryLazy",
     opts = {
