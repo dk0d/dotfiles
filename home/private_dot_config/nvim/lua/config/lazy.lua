4996a26de18c13e4e8d929e09fc1caca3eb546a3
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-02-24 22:49:12 -0500

    refactor lazy init

--- home/private_dot_config/nvim/lua/config/lazy.lua
+++ home/private_dot_config/nvim/lua/config/lazy.lua
@@ -1,28 +1,17 @@
-local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not (vim.env.LAZY or vim.loop.fs_stat(lazypath)) then
-  vim.g.astronvim_first_install = true -- lets AstroNvim know that this is an initial installation
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
-end
-vim.opt.rtp:prepend(lazypath)
-
-local lazy_loaded, lazy = pcall(require, "lazy") -- validate that lazy is available
-if not lazy_loaded then
-  -- stylua: ignore
-  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
-  vim.fn.getchar()
-  vim.cmd.quit()
-end
-
 ---@type LazyConfig
-lazy.setup({
+require("lazy").setup({
   -- TODO: change `branch="v4"` to `version="^4"` on release
-  { "AstroNvim/AstroNvim", branch = "v4", import = "astronvim.plugins" },
-  -- AstroCommunity: import any community modules here
-  -- TODO: Remove branch v4 on release
-  -- { "AstroNvim/astrocommunity", branch = "v4" },
-  -- { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins
+  {
+    "AstroNvim/AstroNvim",
+    branch = "v4",
+    import = "astronvim.plugins",
+    opts = {
+      mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
+      maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
+      icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
+      pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/fal
+    },
+  },
   { import = "plugins" },
 } --[[@as LazySpec]], {
   install = { colorscheme = { "astrodark", "habamax" } },
