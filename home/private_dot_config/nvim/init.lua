7be46bc8ebfcbeebe888123a5c0f1d28bef67249
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-02-24 22:49:18 -0500

    refactor lazy init

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -1,3 +1,21 @@
+-- -----------------------------------------------
+-- Setup lazy
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not (vim.env.LAZY or vim.loop.fs_stat(lazypath)) then
+  vim.g.astronvim_first_install = true -- lets AstroNvim know that this is an initial installation
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_loaded, lazy = pcall(require, "lazy") -- validate that lazy is available
+if not lazy_loaded then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
+end
+
 --- Open a URL under the cursor with the current operating system (Supports Mac OS X and *nix)
 -- @param path the path of the file to open with the system opener
 function SystemOpen(path)
@@ -13,6 +31,8 @@ function SystemOpen(path)
     vim.notify("System open is not supported on this OS!", "error")
   end
 end
+-- Setup lazy
+-- -----------------------------------------------
 
 -- bootstrap lazy.nvim, AstroNvim, and user plugins
 require "config.lazy"
@@ -32,6 +52,7 @@ R = function(name)
   RELOAD(name)
   return require(name)
 end
+
 
 -- run polish file at the very end
 pcall(require, "config.polish")
