280eca886078a6e72ad4756dfb8368e10fa2ec06
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-02-27 01:12:56 -0500

    back to resession

--- home/private_dot_config/astronvim_bak/lua/user/plugins/treesitter.lua
+++ home/private_dot_config/astronvim_bak/lua/user/plugins/treesitter.lua
@@ -1,32 +1,41 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  opts = {
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    ensure_installed = {
-      'lua',
-      'python',
-      'rust',
-      -- "markdown", # causes issues on startup in  linux VMs
-      -- "markdown_inline",
-    },
-    ident = { enable = true },
-    rainbow = {
-      enabled = true,
-      extended_mode = true,
-      max_file_lines = nil,
-    },
-    textobjects = {
-      move = {
-        goto_next_start = {
-          [']s'] = { query = '@class.outer', desc = 'Next class start' },
-        },
-        goto_previous_start = {
-          ['[s'] = { query = '@class.outer', desc = 'Previous class start' },
-        },
-      },
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	opts = function(_, opts)
+		opts = vim.tbl_deep_extend("force", opts or {}, {
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = true,
+			},
+			incremental_selection = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+			ensure_installed = {
+				"lua",
+				"python",
+				"rust",
+				-- "markdown", # causes issues on startup in  linux VMs
+				-- "markdown_inline",
+			},
+			ident = { enable = true },
+			rainbow = {
+				enabled = true,
+				extended_mode = true,
+				max_file_lines = nil,
+			},
+			textobjects = {
+				move = {
+					goto_next_start = {
+						["]s"] = { query = "@class.outer", desc = "Next class start" },
+					},
+					goto_previous_start = {
+						["[s"] = { query = "@class.outer", desc = "Previous class start" },
+					},
+				},
+			},
+		})
+		return opts
+	end,
 }
