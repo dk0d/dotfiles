7966e67f570336ad832cc44cc396624a776604f5
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-02-27 00:33:42 -0500

    move to selene for lua

--- home/private_dot_config/nvim/lua/plugins/mason.lua
+++ home/private_dot_config/nvim/lua/plugins/mason.lua
@@ -25,9 +25,8 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "prettierd",
-        "stylua",
+        "selene",
         -- add more arguments for adding more null-ls sources
-        "stylua",
         "ruff-lsp",
         "rust_analyzer",
         -- 'shellcheck',
