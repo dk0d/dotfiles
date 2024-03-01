21f8980b10a901a098bae6342d37f126b72fb03a
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-03-01 13:13:13 -0500

    fix mdx

--- home/private_dot_config/nvim/lua/config/polish.lua
+++ home/private_dot_config/nvim/lua/config/polish.lua
@@ -2,15 +2,9 @@
 -- augroups/autocommands and custom filetypes also this just pure lua so
 -- anything that doesn't fit in the normal config locations above can go here
 
--- Set up custom filetypes
--- vim.filetype.add {
---   extension = {
---     foo = "fooscript",
---   },
---   filename = {
---     ["Foofile"] = "fooscript",
---   },
---   pattern = {
---     ["~/%.config/foo/.*"] = "fooscript",
---   },
--- }
+vim.filetype.add({
+	extension = { "mdx" },
+})
+
+local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
+ft_to_parser.mdx = "markdown"
