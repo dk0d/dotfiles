7893f29f8568d082eb3ffb3e7f78e3541322154e
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-03-25 00:04:38 -0400

    feat: add pcss filetype

--- home/private_dot_config/nvim/lua/config/polish.lua
+++ home/private_dot_config/nvim/lua/config/polish.lua
@@ -2,7 +2,15 @@
 -- augroups/autocommands and custom filetypes also this just pure lua so
 -- anything that doesn't fit in the normal config locations above can go here
 
+-- MDX
 vim.filetype.add({
 	extension = { mdx = "mdx" },
 })
 vim.treesitter.language.register("markdown", "mdx")
+
+-- PostCSS
+vim.filetype.add({
+	extension = { pcss = "pcss" },
+})
+
+vim.treesitter.language.register("css", "pcss")
