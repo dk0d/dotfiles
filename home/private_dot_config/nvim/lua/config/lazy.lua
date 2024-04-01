550472f137ce6bbb1533c348bcb1a75f1c085d7d
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-04-01 19:52:24 -0400

    fix: astronvim ^4

--- home/private_dot_config/nvim/lua/config/lazy.lua
+++ home/private_dot_config/nvim/lua/config/lazy.lua
@@ -2,11 +2,10 @@
 require("lazy").setup({
 	{
 		"AstroNvim/AstroNvim",
-		branch = "v4",
-		-- version = "^4",
+		version = "^4",
 		import = "astronvim.plugins",
 		opts = {
-			mapleader = " ",   -- This ensures the leader key must be configured before Lazy is set up
+			mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
 			maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
 			icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
 			pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/fal
