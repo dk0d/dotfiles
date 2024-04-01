e889c174ab68e1441f3b7f58ad87462b38e21a67
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-04-01 09:59:50 -0400

    fix: AstroNvim v4 release

--- home/private_dot_config/nvim/lua/config/lazy.lua
+++ home/private_dot_config/nvim/lua/config/lazy.lua
@@ -1,30 +1,30 @@
 ---@type LazyConfig
 require("lazy").setup({
-  -- TODO: change `branch="v4"` to `version="^4"` on release
-  {
-    "AstroNvim/AstroNvim",
-    branch = "v4",
-    import = "astronvim.plugins",
-    opts = {
-      mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
-      maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
-      icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
-      pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/fal
-    },
-  },
-  { import = "plugins" },
+	{
+		"AstroNvim/AstroNvim",
+		version = "^4",
+		import = "astronvim.plugins",
+		opts = {
+			mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
+			maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
+			icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
+			pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/fal
+		},
+	},
+	{ import = "plugins" },
 } --[[@as LazySpec]], {
-  install = { colorscheme = { "astrodark", "habamax" } },
-  performance = {
-    rtp = {
-      -- disable some rtp plugins, add more to your liking
-      disabled_plugins = {
-        "gzip",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "zipPlugin",
-      },
-    },
-  },
+	install = { colorscheme = { "astrodark", "habamax" } },
+	ui = { backdrop = 100 },
+	performance = {
+		rtp = {
+			-- disable some rtp plugins, add more to your liking
+			disabled_plugins = {
+				"gzip",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"zipPlugin",
+			},
+		},
+	},
 } --[[@as LazyConfig]])
