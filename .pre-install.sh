79b14b64d026818408d802db28780fd155887964
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-11-27 22:55:07 -0500

    try this

---
+++ .pre-install.sh
@@ -0,0 +1,14 @@
+echo '🍺  Installing Homebrew';
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+
+if [[ -f "/opt/homebrew/bin/brew" ]]; then
+	eval "$(/opt/homebrew/bin/brew shellenv)";
+	export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:$PATH;
+elif [[ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)";
+	export PATH=/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH;
+fi
+
+brew install --cask 1password
+brew install 1password-cli
