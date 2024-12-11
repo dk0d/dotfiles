4755e11901c84aee8d3f7e1b7c20201df55ea3e9
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-12-10 22:19:13 -0500

    disable nvim-bqf due to errors

--- home/private_dot_config/nvim/lua/plugins/bqf.lua
+++ home/private_dot_config/nvim/lua/plugins/bqf.lua
@@ -1,16 +1,20 @@
 return {
-  {
-    "kevinhwang91/nvim-bqf",
-    event = "VeryLazy",
-    ft = { "qf" },
-    dependencies = {
-      "junegunn/fzf",
-      "nvim-treesitter/nvim-treesitter",
-    },
-  },
-  {
-    "yorickpeterse/nvim-pqf",
-    event = "VeryLazy",
-    config = function(_, opts) require("pqf").setup(opts) end,
-  },
+	-- 	{
+	-- 		"kevinhwang91/nvim-bqf",
+	-- 		enable = false,
+	-- 		event = "VeryLazy",
+	-- 		ft = { "qf" },
+	-- 		dependencies = {
+	-- 			"junegunn/fzf",
+	-- 			"nvim-treesitter/nvim-treesitter",
+	-- 		},
+	-- 	},
+	{
+		"yorickpeterse/nvim-pqf",
+		enable = false,
+		event = "VeryLazy",
+		config = function(_, opts)
+			require("pqf").setup(opts)
+		end,
+	},
 }
