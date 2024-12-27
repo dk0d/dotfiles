5dc6dff9e9526a7f3cbaa3eaf07b0805e6d10075
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-12-27 14:44:12 +0000

    fix: smarter filenames when in a svelte project

--- home/private_dot_config/nvim/lua/plugins/incline.lua
+++ home/private_dot_config/nvim/lua/plugins/incline.lua
@@ -1,3 +1,34 @@
+local detect_project_file = function()
+	local files = {
+		"svelte.config.ts",
+		"svelte.config.js",
+	}
+	local workingdir = vim.fn.getcwd()
+	for _, cfile in ipairs(files) do
+		if require("utils").path_exists(workingdir .. "/" .. cfile) then
+			return true
+		end
+	end
+	return false
+end
+
+local derive_filename = function(bufnr)
+	local fullfilename = vim.api.nvim_buf_get_name(bufnr)
+	local filename = vim.fn.fnamemodify(fullfilename, ":t")
+
+	-- if in svelte/remix project, return relative to routes
+	if detect_project_file() then
+		local routesIdx = string.find(fullfilename, "routes")
+		if routesIdx then
+			filename = string.sub(fullfilename, routesIdx + string.len("routes") + 1)
+		end
+	end
+	if vim.bo[bufnr].modified then
+		filename = "[*]" .. filename
+	end
+	return filename
+end
+
 return {
 	{
 		"b0o/incline.nvim",
@@ -18,10 +49,7 @@ return {
 				window = { margin = { vertical = 0, horizontal = 1 } },
 				hide = { cursorline = true, only_win = true },
 				render = function(props)
-					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-					if vim.bo[props.buf].modified then
-						filename = "[*]" .. filename
-					end
+					local filename = derive_filename(props.buf)
 					local icon, color = require("nvim-web-devicons").get_icon_color(filename)
 					return { { icon, guifg = color }, { " " }, { filename } }
 				end,
