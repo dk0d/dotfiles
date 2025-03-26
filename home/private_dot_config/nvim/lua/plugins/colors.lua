5cab0937c59cd42dcb23ec44a119ff8c21443319
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2025-03-26 18:28:52 -0400

    cleanup colors

--- home/private_dot_config/nvim/lua/plugins/colors.lua
+++ home/private_dot_config/nvim/lua/plugins/colors.lua
@@ -6,97 +6,97 @@ return {
 		config = function()
 			require("catppuccin").setup({
 				flavour = "mocha",
-				custom_highlights = function(colors)
-					local bg = colors.lavender
-					local fg = colors.base
-
-					local secondary_bg = colors.blue
-					local secondary_fg = colors.base
-
-					local muted_bg = colors.overlay0
-					local muted_fg = colors.base
-					return {
-
-						-- NormalFloat = { bg = "none" },
-						-- FloatBorder = { fg = "none" },
-						-- FloatTitle = { bg = "none" },
-						-- Save an hlgroup with dark background and dimmed foreground
-						-- so that you can use it where your still want darker windows.
-						-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-						-- NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-						-- Popular plugins that open floats will link to NormalFloat by default;
-						-- set their background accordingly if you wish to keep them dark and borderless
-						-- LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-
-						MasonHeader = { fg = fg, bg = bg, style = { "bold" } },
-						MasonHeaderSecondary = { fg = secondary_fg, bg = secondary_bg, style = { "bold" } },
-
-						MasonHighlight = { fg = colors.green },
-						MasonHighlightBlock = {
-							bg = colors.green,
-							fg = colors.base,
-						},
-						MasonHighlightBlockBold = { bg = secondary_bg, fg = secondary_fg, bold = true },
-
-						MasonHighlightSecondary = { fg = colors.mauve },
-						MasonHighlightBlockSecondary = { fg = secondary_fg, bg = secondary_bg },
-						MasonHighlightBlockBoldSecondary = { fg = fg, bg = bg, bold = true },
-
-						MasonMuted = { fg = colors.overlay0 },
-						MasonMutedBlock = { bg = muted_bg, fg = muted_fg },
-						MasonMutedBlockBold = { bg = colors.yellow, fg = colors.base, bold = true },
-
-						MasonError = { fg = colors.red },
-
-						MasonHeading = { fg = colors.lavender, bold = true },
-
-						-- even if background is transparent
-						-- TelescopeBorder = {
-						-- 	fg = "none", -- colors.text,
-						-- 	bg = "none", -- colors.mantle,
-						-- },
-						TelescopeMatching = { fg = colors.blue },
-						-- TelescopeNormal = {
-						-- 	bg = "none",
-						-- },
-						-- TelescopePromptBorder = {
-						-- 	fg = "none", -- colors.text,
-						-- 	bg = "none", -- colors.surface0,
-						-- },
-						TelescopePromptNormal = {
-							fg = colors.sky,
-							bg = "none", --colors.surface0,
-						},
-						TelescopePromptPrefix = {
-							fg = colors.flamingo,
-							bg = "none", -- colors.surface0,
-						},
-						TelescopePreviewTitle = {
-							fg = colors.green,
-							bg = "none", -- colors.green,
-						},
-						TelescopePromptTitle = {
-							fg = colors.red,
-							bg = "none", -- colors.red,
-						},
-						-- TelescopeResultsTitle = {
-						-- 	fg = colors.lavender,
-						-- 	bg = "none", -- colors.lavender,
-						-- },
-						TelescopeSelection = {
-							fg = colors.flamingo,
-							bg = colors.mantle, -- colors.surface0,
-							style = { "bold" },
-						},
-						-- TelescopeSelectioncolorsaret = { fg = colors.flamingo },
-
-						Pmenu = { fg = colors.flamingo, bg = colors.base }, -- add `blend = vim.o.pumblend` to enable transparency
-						PmenuSel = { fg = "none", bg = colors.overlay0 },
-						PmenuSbar = { bg = colors.overlay0 },
-						PmenuThumb = { bg = colors.overlay1 },
-					}
-				end,
+				-- custom_highlights = function(colors)
+				-- 	local bg = colors.lavender
+				-- 	local fg = colors.base
+				--
+				-- 	local secondary_bg = colors.blue
+				-- 	local secondary_fg = colors.base
+				--
+				-- 	local muted_bg = colors.overlay0
+				-- 	local muted_fg = colors.base
+				-- 	return {
+				--
+				-- 		-- NormalFloat = { bg = "none" },
+				-- 		-- FloatBorder = { fg = "none" },
+				-- 		-- FloatTitle = { bg = "none" },
+				-- 		-- Save an hlgroup with dark background and dimmed foreground
+				-- 		-- so that you can use it where your still want darker windows.
+				-- 		-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+				-- 		-- NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+				--
+				-- 		-- Popular plugins that open floats will link to NormalFloat by default;
+				-- 		-- set their background accordingly if you wish to keep them dark and borderless
+				-- 		-- LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+				--
+				-- 		MasonHeader = { fg = fg, bg = bg, style = { "bold" } },
+				-- 		MasonHeaderSecondary = { fg = secondary_fg, bg = secondary_bg, style = { "bold" } },
+				--
+				-- 		MasonHighlight = { fg = colors.green },
+				-- 		MasonHighlightBlock = {
+				-- 			bg = colors.green,
+				-- 			fg = colors.base,
+				-- 		},
+				-- 		MasonHighlightBlockBold = { bg = secondary_bg, fg = secondary_fg, bold = true },
+				--
+				-- 		MasonHighlightSecondary = { fg = colors.mauve },
+				-- 		MasonHighlightBlockSecondary = { fg = secondary_fg, bg = secondary_bg },
+				-- 		MasonHighlightBlockBoldSecondary = { fg = fg, bg = bg, bold = true },
+				--
+				-- 		MasonMuted = { fg = colors.overlay0 },
+				-- 		MasonMutedBlock = { bg = muted_bg, fg = muted_fg },
+				-- 		MasonMutedBlockBold = { bg = colors.yellow, fg = colors.base, bold = true },
+				--
+				-- 		MasonError = { fg = colors.red },
+				--
+				-- 		MasonHeading = { fg = colors.lavender, bold = true },
+				--
+				-- 		-- even if background is transparent
+				-- 		-- TelescopeBorder = {
+				-- 		-- 	fg = "none", -- colors.text,
+				-- 		-- 	bg = "none", -- colors.mantle,
+				-- 		-- },
+				-- 		TelescopeMatching = { fg = colors.blue },
+				-- 		-- TelescopeNormal = {
+				-- 		-- 	bg = "none",
+				-- 		-- },
+				-- 		-- TelescopePromptBorder = {
+				-- 		-- 	fg = "none", -- colors.text,
+				-- 		-- 	bg = "none", -- colors.surface0,
+				-- 		-- },
+				-- 		TelescopePromptNormal = {
+				-- 			fg = colors.sky,
+				-- 			bg = "none", --colors.surface0,
+				-- 		},
+				-- 		TelescopePromptPrefix = {
+				-- 			fg = colors.flamingo,
+				-- 			bg = "none", -- colors.surface0,
+				-- 		},
+				-- 		TelescopePreviewTitle = {
+				-- 			fg = colors.green,
+				-- 			bg = "none", -- colors.green,
+				-- 		},
+				-- 		TelescopePromptTitle = {
+				-- 			fg = colors.red,
+				-- 			bg = "none", -- colors.red,
+				-- 		},
+				-- 		-- TelescopeResultsTitle = {
+				-- 		-- 	fg = colors.lavender,
+				-- 		-- 	bg = "none", -- colors.lavender,
+				-- 		-- },
+				-- 		TelescopeSelection = {
+				-- 			fg = colors.flamingo,
+				-- 			bg = colors.mantle, -- colors.surface0,
+				-- 			style = { "bold" },
+				-- 		},
+				-- 		-- TelescopeSelectioncolorsaret = { fg = colors.flamingo },
+				--
+				-- 		Pmenu = { fg = colors.flamingo, bg = colors.base }, -- add `blend = vim.o.pumblend` to enable transparency
+				-- 		PmenuSel = { fg = "none", bg = colors.overlay0 },
+				-- 		PmenuSbar = { bg = colors.overlay0 },
+				-- 		PmenuThumb = { bg = colors.overlay1 },
+				-- 	}
+				-- end,
 				transparent_background = true,
 				-- dim_inactive = {
 				-- 	enabled = false, -- dims the background color of inactive window
@@ -104,28 +104,14 @@ return {
 				-- 	percentage = 0.15, -- percentage of the shade to apply to the inactive window
 				-- },
 				term_colors = false,
-				color_overrides = {
-					-- all = catppuccin_onedark,
-					-- all = {
-					-- 		subtext1 = monokai.dimmed1,
-					-- 		subtext0 = monokai.dimmed2,
-					-- 		overlay2 = monokai.dimmed3,
-					-- 		overlay1 = monokai.dimmed4,
-					-- 		overlay0 = monokai.dimmed5,
-					-- 		surface2 = monokai.dimmed6,
-					-- 		surface1 = monokai.dimmed7,
-					-- 		surface0 = monokai.dimmed8,
-					-- 		base = monokai.background,
-					-- 		crust = monokai.dark1,
-					-- 		mantle = monokai.dark2,
-					-- },
-				},
 				integrations = {
 					alpha = true,
+					blink_cmp = true,
 					dashboard = true,
 					dap = true,
 					dap_ui = true,
 					harpoon = true,
+					diffview = true,
 					flash = true,
 					gitsigns = true,
 					illuminate = true,
@@ -162,6 +148,10 @@ return {
 					neotest = true,
 					noice = true,
 					notify = true,
+					snacks = {
+						enabled = true,
+						indent_scope_color = "text",
+					},
 					semantic_tokens = true,
 					telescope = {
 						enabled = true,
