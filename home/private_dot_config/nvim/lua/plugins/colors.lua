bffbe6cdc1319e807258dacadfeae1a0bacf8bdd
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2025-04-29 07:10:22 -0400

    color updates

--- home/private_dot_config/nvim/lua/plugins/colors.lua
+++ home/private_dot_config/nvim/lua/plugins/colors.lua
@@ -68,6 +68,43 @@ return {
 		config = function()
 			require("catppuccin").setup({
 				flavour = "mocha",
+				color_overrides = {
+					all = {
+						-- this 16 colors are changed to onedark
+						base = "#282c34",
+						mantle = "#353b45",
+						surface0 = "#3e4451",
+						surface1 = "#545862",
+						surface2 = "#565c64",
+						text = "#abb2bf",
+						rosewater = "#b6bdca",
+						lavender = "#c8ccd4",
+						red = "#e06c75",
+						peach = "#d19a66",
+						yellow = "#e5c07b",
+						green = "#98c379",
+						teal = "#56b6c2",
+						blue = "#61afef",
+						mauve = "#c678dd",
+						flamingo = "#be5046",
+
+						-- now patching extra palettes
+						maroon = "#e06c75",
+						sky = "#d19a66",
+
+						-- extra colors not decided what to do
+						pink = "#F5C2E7",
+						sapphire = "#74C7EC",
+
+						subtext1 = "#BAC2DE",
+						subtext0 = "#A6ADC8",
+						overlay2 = "#9399B2",
+						overlay1 = "#7F849C",
+						overlay0 = "#6C7086",
+
+						crust = "#11111B",
+					},
+				},
 				-- custom_highlights = function(colors)
 				-- 	local bg = colors.lavender
 				-- 	local fg = colors.base
@@ -172,6 +209,7 @@ return {
 					dashboard = true,
 					dap = true,
 					dap_ui = true,
+					dadbod_ui = true,
 					harpoon = true,
 					diffview = true,
 					flash = true,
@@ -184,6 +222,7 @@ return {
 					lsp_trouble = true,
 					markdown = true,
 					mason = true,
+					render_markdown = true,
 					native_lsp = {
 						enabled = true,
 						virtual_text = {
