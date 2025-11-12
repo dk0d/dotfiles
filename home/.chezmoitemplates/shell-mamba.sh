c7212ebd734f32921ea866a0815a04aaaba3c9bc
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2025-11-12 08:03:17 -0500

    fix: faster zsh startup

--- home/.chezmoitemplates/shell-mamba.sh
+++ home/.chezmoitemplates/shell-mamba.sh
@@ -1,5 +1,6 @@
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
+init_mamba() {
 __conda_setup="$("$HOME/.miniforge/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
@@ -24,4 +25,5 @@ else
     alias mamba="$MAMBA_EXE"  # Fallback on help from mamba activate
 fi
 unset __mamba_setup
+}
 # <<< mamba initialize <<<
