cc81f232dc51389c098fba3bd4909be915355f68
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2026-08-25 20:03:20 -0400

    fix treesitter conflict

--- home/private_dot_config/nvim/lua/plugins/ts-installer.lua
+++ home/private_dot_config/nvim/lua/plugins/ts-installer.lua
@@ -1,49 +1,55 @@
 return {
-  "romus204/tree-sitter-manager.nvim",
-  dependencies = {}, -- tree-sitter CLI must be installed system-wide
-  config = function()
-    require("tree-sitter-manager").setup({
-      -- Default Options
-      ensure_installed = {
-        "lua",
-        "vim",
-        "python",
-        "rust",
-        -- "swift", -- remove on linux
-        "markdown",
-        "typescript",
-        "javascript",
-        "svelte",
-        "sql",
-        "html",
-        "tsx",
-        "bash",
-        "css",
-        "jsdoc",
-        "json",
-        "toml",
-        "zig",
-        "csv",
-        "go",
-        "fish",
-        "yaml",
-        "dockerfile",
-        "fga",
-      }, -- list of parsers to install at the start of a neovim session
-      -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
-      -- auto_install = false, -- if enabled, install missing parsers when editing a new file
-      -- highlight = true, -- treesitter highlighting is enabled by default
-      languages = {
-        fga = {
-          install_info = {
-            url = "https://github.com/dk0d/tree-sitter-fga/",
-            branch = "main",
-            use_repo_queries = true,
+  -- tree-sitter-manager owns parser installs; AstroNvim's nvim-treesitter would
+  -- fight over site/parser (E5108 on update from missing parser-info revisions)
+  { "nvim-treesitter/nvim-treesitter", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  {
+    "romus204/tree-sitter-manager.nvim",
+    dependencies = {}, -- tree-sitter CLI must be installed system-wide
+    config = function()
+      require("tree-sitter-manager").setup({
+        -- Default Options
+        ensure_installed = {
+          "lua",
+          "vim",
+          "python",
+          "rust",
+          -- "swift", -- remove on linux
+          "markdown",
+          "typescript",
+          "javascript",
+          "svelte",
+          "sql",
+          "html",
+          "tsx",
+          "bash",
+          "css",
+          "jsdoc",
+          "json",
+          "toml",
+          "zig",
+          "csv",
+          "go",
+          "fish",
+          "yaml",
+          "dockerfile",
+          "fga",
+        }, -- list of parsers to install at the start of a neovim session
+        -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
+        auto_install = true, -- if enabled, install missing parsers when editing a new file
+        -- highlight = true, -- treesitter highlighting is enabled by default
+        languages = {
+          fga = {
+            install_info = {
+              url = "https://github.com/dk0d/tree-sitter-fga/",
+              branch = "main",
+              use_repo_queries = true,
+            },
           },
-        },
-      }, -- override or add new parser sources
-      -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
-      -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
-    })
-  end,
+        }, -- override or add new parser sources
+        -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
+        -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
+      })
+    end,
+  },
 }
