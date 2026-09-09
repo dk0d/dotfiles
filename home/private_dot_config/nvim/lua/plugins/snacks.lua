d04c454dec57ef324be502291b27943f85897dc9
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2026-09-08 20:24:37 -0400

    snacks cleanup

--- home/private_dot_config/nvim/lua/plugins/snacks.lua
+++ home/private_dot_config/nvim/lua/plugins/snacks.lua
@@ -140,7 +140,7 @@ return {
       },
       sections = {
         -- { section = "terminal", cmd = "neofetch -L", height = 50, width = 50, padding = 2, gap = 1 },
-        { section = "keys", gap = 1, padding = 1 },
+        { section = "keys",   gap = 1, padding = 1 },
         { section = "startup" },
         {
           pane = 2,
@@ -152,58 +152,58 @@ return {
             Snacks.gitbrowse()
           end,
         },
-        function()
-          local in_git = Snacks.git.get_root() ~= nil
-          local cmds = {
-            -- {
-            -- 	title = "Review Requests",
-            -- 	cmd = "gh search prs --review-requested @me --state open -L 5",
-            -- 	action = function()
-            -- 		vim.ui.open("https://github.com/notifications")
-            -- 	end,
-            -- 	key = "n",
-            -- 	icon = " ",
-            -- 	height = 5,
-            -- 	enabled = true,
-            -- },
-            {
-              icon = " ",
-              title = "Open PRs",
-              cmd = "gh pr list -L 5",
-              key = "P",
-              action = function()
-                vim.fn.jobstart("gh pr list --web", { detach = true })
-              end,
-              height = 10,
-            },
-            {
-              title = "Open Issues",
-              cmd = "gh issue list -L 5",
-              key = "i",
-              action = function()
-                vim.fn.jobstart("gh issue list --web", { detach = true })
-              end,
-              icon = " ",
-              height = 10,
-            },
-            -- {
-            -- 	icon = " ",
-            -- 	title = "Git Status",
-            -- 	cmd = "git --no-pager diff --stat -B -M -C",
-            -- 	height = 10,
-            -- },
-          }
-          return vim.tbl_map(function(cmd)
-            return vim.tbl_extend("force", {
-              pane = 2,
-              section = "terminal",
-              enabled = in_git,
-              padding = 1,
-              ttl = 5 * 60,
-              indent = 3,
-            }, cmd)
-          end, cmds)
-        end,
+        -- function()
+        --   local in_git = Snacks.git.get_root() ~= nil
+        --   local cmds = {
+        --     -- {
+        --     -- 	title = "Review Requests",
+        --     -- 	cmd = "gh search prs --review-requested @me --state open -L 5",
+        --     -- 	action = function()
+        --     -- 		vim.ui.open("https://github.com/notifications")
+        --     -- 	end,
+        --     -- 	key = "n",
+        --     -- 	icon = " ",
+        --     -- 	height = 5,
+        --     -- 	enabled = true,
+        --     -- },
+        --     {
+        --       icon = " ",
+        --       title = "Open PRs",
+        --       cmd = "gh pr list -L 5",
+        --       key = "P",
+        --       action = function()
+        --         vim.fn.jobstart("gh pr list --web", { detach = true })
+        --       end,
+        --       height = 10,
+        --     },
+        --     {
+        --       title = "Open Issues",
+        --       cmd = "gh issue list -L 5",
+        --       key = "i",
+        --       action = function()
+        --         vim.fn.jobstart("gh issue list --web", { detach = true })
+        --       end,
+        --       icon = " ",
+        --       height = 10,
+        --     },
+        --     -- {
+        --     -- 	icon = " ",
+        --     -- 	title = "Git Status",
+        --     -- 	cmd = "git --no-pager diff --stat -B -M -C",
+        --     -- 	height = 10,
+        --     -- },
+        --   }
+        --   return vim.tbl_map(function(cmd)
+        --     return vim.tbl_extend("force", {
+        --       pane = 2,
+        --       section = "terminal",
+        --       enabled = in_git,
+        --       padding = 1,
+        --       ttl = 5 * 60,
+        --       indent = 3,
+        --     }, cmd)
+        --   end, cmds)
+        -- end,
       },
     },
     image = { enable = true },
